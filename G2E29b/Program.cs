af852928473fe89cf1fa7a3946c0b3689fcee2ad
Mauricio Orozco <0rozkoo.armada@gmail.com>
2016-03-28 14:38:52 -0600

    G2E29

---
+++ G2E29b/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G2E29b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bis = 366,min=0;
+            min = bis * 24 * 60;
+
+            Console.WriteLine("Hay {0} minutos en un año bisiesto", min);
+            Console.ReadKey();
+        }
+    }
+}
